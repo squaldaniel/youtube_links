077884a6e9242364afd8ecab5ba31eeea205c292
Daniels Hogans <daniel.santos.ap@gmail.com>
2026-01-05 22:08:43 -0300

    +1

---
+++ Como Montar um setup de programação rapidamente.ps1
@@ -0,0 +1,13 @@
+# choco install googlechrome /y
+choco install lightshot /y
+choco install vlc /y
+choco install foxitreader /y
+choco install php --version=8.2 /y
+choco install composer /y
+choco install git /y
+choco install vscode /y
+choco install nodejs --version 22.8.0 /y
+# choco install wsl2 /y
+# choco install wsl-debiangnulinux /y
+# choco install obs-studio /y
+#
