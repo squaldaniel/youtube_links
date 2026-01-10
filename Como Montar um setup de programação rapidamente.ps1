b87ca72be80f49f9d8eac9c84f337b630262e622
Daniels Hogans <daniel.santos.ap@gmail.com>
2026-01-10 20:48:27 -0300

    update progs

--- Como Montar um setup de programação rapidamente.ps1
+++ Como Montar um setup de programação rapidamente.ps1
@@ -7,6 +7,7 @@ choco install composer /y
 choco install git /y
 choco install vscode /y
 choco install nodejs --version 22.8.0 /y
+choco install insomnia-rest-api-client /y
 # choco install wsl2 /y
 # choco install wsl-debiangnulinux /y
 # choco install obs-studio /y
