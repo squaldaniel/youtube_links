ba339a483415e52271b7832c9551f0a37cfcc517
Daniels Hogans <daniel.santos.ap@gmail.com>
2026-02-03 22:33:12 -0300

    update install.ps1 fix

--- install_choco.bat
+++ install_choco.bat
@@ -1,3 +1,6 @@
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# powershell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass"
+powershell -ExecutionPolicy Bypass -File "install.ps1"
+
 powershell -File install.ps1
